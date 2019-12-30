16394e3ced37a6436052ff1154b44d92623fb8c5
Muyinda Rogers <moverr@gmail.com>
2019-12-30 19:16:40 +0300

    :Debgging

--- app.py
+++ app.py
@@ -30,6 +30,74 @@ classifier_indexes = []
 midvote = 0
 
 
+class VoteClassifier(ClassifierI):
+    # List of classifiers passsed to this
+    def __init__(self, *classifiers):
+        self._classifiers = classifiers
+
+    def classify(self, features):
+        votes = []
+        index = 0
+        for c in self._classifiers:
+            v = c.classify(features)
+            index = index + 1
+            votes.append(v)
+            classifier_indexes.append(index)
+
+        return mode(votes)
+
+    def confidence(self, features):
+        votes = []
+        for c in self._classifiers:
+            v = c.classify(features)
+            votes.append(v)
+        choice_votes = votes.count(mode(votes))
+        conf = choice_votes/len(votes)
+        return conf
+
+    def predict(self, text):
+        pass
+
+
+def remove_noise(tweet_tokens, stop_words=()):
+    lemmetizer = WordNetLemmatizer()
+    cleaned_tokens = []
+
+    for token, tag in pos_tag(tweet_tokens):
+        token = re.sub('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+#]|[!*\(\),]|'
+                       '(?:%[0-9a-fA-F][0-9a-fA-F]))+', '', token)
+        token = re.sub("(@[A-Za-z0-9_]+)", "", token)
+
+        if tag.startswith("NN"):
+            pos = 'n'
+        elif tag.startswith('VB'):
+            pos = 'v'
+        else:
+            pos = 'a'
+
+        lemmatizer = WordNetLemmatizer()
+        token = lemmatizer.lemmatize(token, pos)
+
+        if len(token) > 0 and token not in string.punctuation and token.lower() not in stop_words:
+            # // we are chainge words back to the original flow
+            lametizedword = lemmetizer.lemmatize(token.lower())
+            cleaned_tokens.append(lametizedword)
+    return cleaned_tokens
+
+
+def get_all_words(cleaned_tokens_list):
+    for tokens in cleaned_tokens_list:
+        for token in tokens:
+            yield token
+
+
+def get_tweets_for_model(cleaned_tokens_list):
+    for tweet_tokens in cleaned_tokens_list:
+        yield dict([token, True] for token in tweet_tokens)
+
+
+
+
 
 
 
